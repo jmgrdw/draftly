32e1bcf9f485fe0039518c73933fc61e92715dd8
mcarthurgill <mickgill2008@gmail.com>
2013-08-26 16:07:03 -0700

    player_show spec

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Player do 
-	context "creation" do 
-		let(:player) { Player.new(full_name: "McArthur Gill", espn_url: "www.test.com", team_id: 1, position: "PG", salary: 9500, position_value: 1) }
+	let(:player) { Player.new(full_name: "McArthur Gill", espn_url: "www.test.com", team_id: 1, position: "PG", salary: 9500, position_value: 1) }
+	let(:team) { Team.new(full_name: "Boston Celtics", abbreviation: "bos", espn_long_name: "boston-celtics", url: "espn.com/boston", city: "Boston", short_name: "Celtics", playing_today: true) }
 
+	context "creation" do 
 		it "should create with valid attributes" do 
 			player.save
 			expect(Player.count).to eq(1)
@@ -32,5 +33,18 @@ describe Player do
 		  player.save
 		  expect(Player.count).to eq(0)
 		end
+	end
+
+	context "name" do 
+		it "should return the full_name of the player" do
+		  expect(player.name).to eq(player.full_name)
+		end
+	end
+
+	context "player_show" do
+	  it "should return the team abbreviation and the player name" do
+	  	team.save
+	    expect(player.player_show).to eq("(BOS) McArthur Gill")
+	  end
 	end
 end
